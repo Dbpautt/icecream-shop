717295899fec80d3608ec2196e94098f1e5faa9a
Diana Blanco <dbpautt@gmail.com>
2018-10-12 11:34:59 +0200

    added style

---
+++ src/app/pages/icecream-list-page/icecream-list-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { IcecreamService } from '../../services/icecream.service';
+
+@Component({
+  selector: 'app-icecream-list-page',
+  templateUrl: './icecream-list-page.component.html',
+  styleUrls: ['./icecream-list-page.component.scss']
+})
+export class IcecreamListPageComponent implements OnInit {
+
+  icecreams: Array<any>;
+  error = false;
+
+  constructor(private icecreamService: IcecreamService) { }
+
+  ngOnInit() {
+    this.icecreamService.getAll()
+    .then((results) => {
+      this.icecreams = results;
+    })
+    .catch((error) => {
+      console.log(error);
+      this.error = true;
+    });
+  }
+
+}
+
+
